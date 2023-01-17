347165b428f317adc728bd78aa8491fdbc1237e1
anzu-4 <kei_21n1101757@nnn.ed.jp>
2023-01-18 00:20:45 +0900

    22

--- newclock.js
+++ newclock.js
@@ -20,7 +20,7 @@ function clock() {
     const minute = twoDigit(now.getMinutes());
     const second = twoDigit(now.getSeconds());
 
-    if(minute == 17 && second == 00){
+    if(minute == 22 && second == 00){
         audio()
     }
     document.getElementById("clock_date").textContent = year + "/" + month + "/" + day + " (" + week + ")";
