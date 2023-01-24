d0aa0a2503276cf0a7e44cbfe970828e2d3e647e
anzu-4 <kei_21n1101757@nnn.ed.jp>
2023-01-24 16:25:41 +0900

    Update sound.js

--- sound.js
+++ sound.js
@@ -1,11 +1,11 @@
 function audio() {
-    new Audio("jiho-kurakazu.mp3").play()
+    new Audio("./jiho-kurakazu.mp3").play()
  //   document.getElementById('btn_audio').currentTime = 0; //連続クリックに対応
   //  document.getElementById('btn_audio').play(); //クリックしたら音を再生
 }
 function hide() {
     for(let i=0;i<100;i++){
-    new Audio("jiho-kurakazu.mp3").play()
+    new Audio("./jiho-kurakazu.mp3").play()
 }
     
 }
