984698a3edc05ae1d3d50cc671f6ff00444069dc
anzu-4 <kei_21n1101757@nnn.ed.jp>
2023-01-26 14:46:07 +0900

    重ねて再生テスト

--- sound.js
+++ sound.js
@@ -3,10 +3,12 @@ function audio() {
  //   document.getElementById('btn_audio').currentTime = 0; //連続クリックに対応
   //  document.getElementById('btn_audio').play(); //クリックしたら音を再生
 }
+// const kurakazu =  new Audio("./jiho-kurakazu.mp3")
+//     for (let i = 0; i < 30; i++) {
+//        kurakazu.play()
 function hide() {
-    const kurakazu =  new Audio("./jiho-kurakazu.mp3")
-    for(let i=0;i<100;i++){
-    kurakazu.play()
-}
+    for (let i = 0; i < 30; i++) {
+      new Audio("./jiho-kurakazu.mp3").play()
+    }
+  }
     
-}
